1281ee3bf83099a0a92d373c89418f2ec9025118
Sanchezguidorl <sanchezguidorl@gmail.com>
2023-09-01 10:04:28 -0300

    Se crea componente Header

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes } from 'react-router-dom'
+import '../styles/main.scss'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
