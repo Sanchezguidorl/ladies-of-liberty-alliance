b4d0ab1730d9cffdb639740f194a8203811308f4
Sanchezguidorl <sanchezguidorl@gmail.com>
2023-09-01 10:38:31 -0300

    Se agregan elementos para navbar mediante bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes} from 'react-router-dom'
 import '../styles/main.scss'
+import Header from './layouts/Header'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         
       </Routes>
