515a66f70b30d2a5d7a008c0b436953f35888541
Sanchezguidorl <sanchezguidorl@gmail.com>
2023-09-07 00:03:18 -0300

    se comentan importaciones no usadas para evitar error de netlify

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -2,6 +2,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import Banner from "./../layouts/Banner";
 import imagen from "../assets/images/img-lola.jpg";
 import img1 from "../assets/images/img1.jpg";
+/*
 import img2 from "../assets/images/img2.jpg";
 import img3 from "../assets/images/img3.jpg";
 import img4 from "../assets/images/img4.jpg";
@@ -19,7 +20,7 @@ import img15 from "../assets/images/img15.jpg";
 import img16 from "../assets/images/img16.jpg";
 import img17 from "../assets/images/img17.jpg";
 import img18 from "../assets/images/img18.jpg";
-
+*/
 function AboutUs() {
   return (
     <div>
